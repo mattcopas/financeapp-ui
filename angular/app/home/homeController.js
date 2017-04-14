16dd8645d525b6e2280b7319bbdbc4f55a499417
Matt Copas <Matt Copas>
2017-04-14 18:55:50 +0100

    Adding non functional accounts table to home page

--- angular/app/home/homeController.js
+++ angular/app/home/homeController.js
@@ -4,8 +4,9 @@ financeApp.controller('homeController', ['accountService', '$scope', '$rootScope
 
   accountService.getAccountsByUserId(1).then(function success(response) {
     $scope.accounts = response.data;
+    console.log("$scope.accounts ", $scope.accounts);
   }, function errorCallBack(response) {
-
+    console.log("Error getting data: ", response.data);
   });
 
 }]);

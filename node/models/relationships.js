ed6cbb50a0170b23c1e5fec86bcfe6f8651f9470
Matt <mattwfcopas@gmail.com>
2017-05-21 15:23:33 +0100

    Removing redundant node stuff

--- node/models/relationships.js
+++
@@ -1,8 +0,0 @@
-var Account = require('./account');
-var Transaction = require('./transaction');
-
-
-module.exports = function() {
-  Account.belongsToMany(Transaction, {through: 'AccountTransaction'});
-  Transaction.belongsToMany(Account, {through: 'AccountTransaction'});
-};
